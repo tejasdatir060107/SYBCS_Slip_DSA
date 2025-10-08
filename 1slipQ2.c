e40876079ac35906da2b12c86a0565289234e07d
Tejas Datir <tejasdatir2021@gmail.com>
2025-10-08 17:53:17 +0530

    1slipQ2.c

---
+++ 1slipQ2.c
@@ -0,0 +1,70 @@
+/* Write a program that sorts the elements of linked list using any of sorting 
+technique. */
+
+
+#include<stdio.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node * create(struct node *head)
+{
+    int i,n;
+    struct node *newnode,*temp;
+    printf("enter limit:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("Enter value:");
+        scanf("%d",&newnode->data);
+        newnode->next=NULL;
+
+        if(head==NULL)
+        {
+            head=temp=newnode;
+        }
+        else
+        {
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+    return head;
+}
+struct node *sorted(struct node*head)
+{
+    struct node *i,*j;
+    int t;
+    for(i=head;i!=NULL;i=i->next)
+    {
+        for(j=head;j!=i;j=j->next)
+        {
+            if(j->data>i->data)
+            {
+                t=j->data;
+                j->data=i->data;
+                i->data=t;
+            }
+        }
+    }
+
+    return head;
+}
+void disp(struct node * head)
+{
+    struct node *temp;
+    for(temp=head;temp!=NULL;temp=temp->next)
+    {
+        printf("%d\t",temp->data);
+    }
+}
+int main()
+{
+    struct node *head;
+    head=create(NULL);
+    printf("\nsorted linkedlist:");
+    head=sorted(head);
+    disp(head);
+}
